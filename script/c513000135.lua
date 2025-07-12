593f5b4f947da1fc347c8b22086cc5b7be95ea27
jinyouli <229338934@qq.com>
2025-07-12 08:59:47 +0800

    修复木龙兽效果

--- script/c513000135.lua
+++ script/c513000135.lua
@@ -14,10 +14,14 @@ function c513000135.initial_effect(c)
 
 	--summon success
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetOperation(c513000135.regop)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(0,1)
+	e1:SetValue(c513000135.aclimit)
 	c:RegisterEffect(e1)
-	e1:SetLabelObject(e1)
+
 
 	--act limit
 	local e2=Effect.CreateEffect(c)
@@ -26,7 +30,7 @@ function c513000135.initial_effect(c)
 	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetTargetRange(0,1)
-	e2:SetValue(c513000135.aclimit)
+	e2:SetValue(c513000135.acmonsterlimit)
 	c:RegisterEffect(e2)
 end
 
@@ -53,22 +57,11 @@ function c513000135.hspop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Release(tc,REASON_SPSUMMON)
 end
 
-function c513000135.aclimit(e,re,tp)
+function c513000135.acmonsterlimit(e,re,tp)
 	return re:IsActiveType(TYPE_MONSTER)
 end
 
 
-function c513000135.regop(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
-	e1:SetTargetRange(0,1)
-	e1:SetValue(c513000135.aclimit)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	e:GetHandler():RegisterEffect(e1)
-end
 function c513000135.aclimit(e,re,tp)
 	return re:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
