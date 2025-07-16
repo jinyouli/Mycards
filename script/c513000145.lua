b32675be6dbfe683eff1f0ae3620993e1c1a10e8
jinyouli <229338934@qq.com>
2025-07-17 01:09:02 +0800

    电子龙无限

---
+++ script/c513000145.lua
@@ -0,0 +1,84 @@
+--サイバー・ドラゴン・インフィニティ
+function c513000145.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,c513000145.mfilter,8,2,c513000145.ovfilter,aux.Stringid(93039339,0),3,c513000145.xyzop)
+	c:EnableReviveLimit()
+	--atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(c513000145.atkval)
+	c:RegisterEffect(e1)
+	--material
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c513000145.target)
+	e2:SetOperation(c513000145.operation)
+	c:RegisterEffect(e2)
+	--negate
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(c513000145.discon)
+	e3:SetTarget(c513000145.distg)
+	e3:SetOperation(c513000145.disop)
+	c:RegisterEffect(e3)
+end
+function c513000145.mfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_MONSTER)
+end
+function c513000145.ovfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_EFFECT)
+end
+function c513000145.xyzop(e,tp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,513000145)==0 end
+	Duel.RegisterFlagEffect(tp,513000145,RESET_PHASE+PHASE_END,EFFECT_FLAG_OATH,1)
+end
+function c513000145.atkval(e,c)
+	return c:GetOverlayCount()*200
+end
+function c513000145.filter(c)
+	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsCanOverlay()
+end
+function c513000145.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c513000145.filter(chkc) and chkc~=e:GetHandler() end
+	if chk==0 then return e:GetHandler():IsType(TYPE_XYZ)
+		and Duel.IsExistingTarget(c513000145.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	Duel.SelectTarget(tp,c513000145.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,e:GetHandler())
+end
+function c513000145.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+		local og=tc:GetOverlayGroup()
+		if og:GetCount()>0 then
+			Duel.SendtoGrave(og,REASON_RULE)
+		end
+		Duel.Overlay(c,Group.FromCards(tc))
+	end
+end
+function c513000145.discon(e,tp,eg,ep,ev,re,r,rp)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev) and ep==1-tp
+end
+
+function c513000145.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function c513000145.disop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
