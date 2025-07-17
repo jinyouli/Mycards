c621f376f2ea8dae2dc45ef0d6af3da2a392684a
jinyouli <229338934@qq.com>
2025-07-18 00:58:08 +0800

    三幻神

---
+++ script/c513000147.lua
@@ -0,0 +1,167 @@
+--オシリスの天空竜
+function c513000147.initial_effect(c)
+	--summon with 3 tribute
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(10000020,2))
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_LIMIT_SUMMON_PROC)
+	e1:SetCondition(c513000147.ttcon)
+	e1:SetOperation(c513000147.ttop)
+	e1:SetValue(SUMMON_TYPE_ADVANCE)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_LIMIT_SET_PROC)
+	e2:SetCondition(c513000147.setcon)
+	c:RegisterEffect(e2)
+	--summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_CANNOT_DISABLE_SUMMON)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	c:RegisterEffect(e3)
+	--summon success
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_SUMMON_SUCCESS)
+	e4:SetOperation(c513000147.sumsuc)
+	c:RegisterEffect(e4)
+	--to grave
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(10000020,0))
+	e5:SetCategory(CATEGORY_TOGRAVE)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1)
+	e5:SetCode(EVENT_PHASE+PHASE_END)
+	e5:SetCondition(c513000147.tgcon)
+	e5:SetTarget(c513000147.tgtg)
+	e5:SetOperation(c513000147.tgop)
+	c:RegisterEffect(e5)
+	--atk/def
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetCode(EFFECT_UPDATE_ATTACK)
+	e6:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetValue(c513000147.adval)
+	c:RegisterEffect(e6)
+	local e7=e6:Clone()
+	e7:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e7)
+
+
+	--unaffectable
+    local e7=Effect.CreateEffect(c)
+    e7:SetType(EFFECT_TYPE_SINGLE)
+    e7:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e7:SetRange(LOCATION_MZONE)
+    e7:SetCode(EFFECT_IMMUNE_EFFECT)
+    e7:SetValue(c513000147.efilter)
+    c:RegisterEffect(e7)
+	
+
+	--下降对手怪兽攻击、破坏
+	local e10=Effect.CreateEffect(c)
+	e10:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
+	e10:SetDescription(aux.Stringid(10000020,1))
+	e10:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
+	e10:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e10:SetRange(LOCATION_MZONE)
+	e10:SetCountLimit(10)
+	e10:SetCode(EVENT_SUMMON_SUCCESS)
+	e10:SetCondition(c513000147.atkcon)
+	e10:SetTarget(c513000147.atktg)
+	e10:SetOperation(c513000147.atkop)
+	c:RegisterEffect(e10)
+	local e11=e10:Clone()
+	e11:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e11)
+	local e12=e11:Clone()
+	e12:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e12)
+end
+
+function c513000147.efilter(e,te)
+    return te:GetOwner()~=e:GetOwner()
+end
+
+function c513000147.ttcon(e,c,minc)
+	if c==nil then return true end
+	return minc<=3 and Duel.CheckTribute(c,3)
+end
+function c513000147.ttop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectTribute(tp,c,3,3)
+	c:SetMaterial(g)
+	Duel.Release(g,REASON_SUMMON+REASON_MATERIAL)
+end
+function c513000147.setcon(e,c,minc)
+	if not c then return true end
+	return false
+end
+function c513000147.sumsuc(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetChainLimitTillChainEnd(aux.FALSE)
+end
+function c513000147.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+function c513000147.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,e:GetHandler(),1,0,0)
+end
+function c513000147.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		Duel.SendtoGrave(c,REASON_EFFECT)
+	end
+end
+function c513000147.adval(e,c)
+	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_HAND,0)*1000
+end
+function c513000147.atkfilter(c,tp)
+	return c:IsControler(tp) and c:IsPosition(POS_FACEUP)
+end
+
+function c513000147.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c513000147.atkfilter,1,nil,1-tp)
+end
+
+function c513000147.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return eg:IsContains(chkc) and s.atkfilter(chkc,tp) end
+	if chk==0 then return e:GetHandler():IsRelateToEffect(e) end
+	Duel.SetTargetCard(eg:Filter(c513000147.atkfilter,nil,1-tp))
+end
+
+function c513000147.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local dg=Group.CreateGroup()
+	local c=e:GetHandler()
+	if g:GetCount()>0 then
+	local tc=g:GetFirst()
+	while tc do
+		local preatk=tc:GetAttack()
+		local predef=tc:GetDefense()
+		if tc:GetPosition()==POS_FACEUP_ATTACK and preatk>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-2000)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+		if tc:GetAttack()==0 then dg:AddCard(tc) end end
+
+		if tc:GetPosition()==POS_FACEUP_DEFENSE and predef>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_UPDATE_DEFENSE)
+		e1:SetValue(-2000)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+		if tc:GetDefense()==0 then dg:AddCard(tc) end end
+		tc=g:GetNext()
+	end
+	Duel.Destroy(dg,REASON_RULE) end
+end
