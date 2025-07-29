2b68c87295b0095c5f8579de2e7069d599d7820f
jinyouli <229338934@qq.com>
2025-07-29 23:16:17 +0800

    祖母暗之书

--- script/c77239021.lua
+++ script/c77239021.lua
@@ -1,6 +1,6 @@
 --祖母暗之书
 function c77239021.initial_effect(c)
-	--从卡组选择3张卡加入手牌
+	--从卡组\墓地选择3张卡加入手牌
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(77239021,0))	
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -10,24 +10,15 @@ function c77239021.initial_effect(c)
 	e1:SetOperation(c77239021.activate)
 	c:RegisterEffect(e1)
 	
-	--从墓地选择3张卡加入手牌
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(77239021,1))
-	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e2:SetType(EFFECT_TYPE_ACTIVATE)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetTarget(c77239021.target2)
-	e2:SetOperation(c77239021.activate2)
-	c:RegisterEffect(e2)
 end
 --------------------------------------------------------------
 function c77239021.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_DECK,0,3,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_DECK+LOCATION_GRAVE,0,3,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function c77239021.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(77239021,0))
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_DECK,0,3,3,nil)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_DECK+LOCATION_GRAVE,0,3,3,nil)
 	Duel.SendtoHand(g,nil,REASON_EFFECT)
 end
 --------------------------------------------------------------
