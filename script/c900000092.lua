ba5bf8aa1325990444ac984905a54e5bdc1177e9
jinyouli <229338934@qq.com>
2025-09-07 08:28:44 +0800

    修改bug

--- script/c900000092.lua
+++ script/c900000092.lua
@@ -29,8 +29,9 @@ function c900000092.initial_effect(c)
 
 	--copy spell
 	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_ACTIVATE)
+	e5:SetType(EFFECT_TYPE_IGNITION)
 	e5:SetCode(EVENT_FREE_CHAIN)
+	e5:SetRange(LOCATION_MZONE)
 	e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e5:SetTarget(c900000092.target)
 	e5:SetCountLimit(1)
@@ -40,9 +41,10 @@ function c900000092.initial_effect(c)
 	--special summon
 	local e6=Effect.CreateEffect(c)
 	e6:SetDescription(aux.Stringid(900000092,0))
-	e6:SetType(EFFECT_TYPE_ACTIVATE)
+	e6:SetType(EFFECT_TYPE_IGNITION)
 	e6:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e6:SetCode(EVENT_FREE_CHAIN)
+	e6:SetRange(LOCATION_MZONE)
 	e6:SetCountLimit(1)
 	e6:SetTarget(c900000092.sptg)
 	e6:SetOperation(c900000092.spop)
@@ -63,7 +65,6 @@ function c900000092.initial_effect(c)
 	local e8=Effect.CreateEffect(c)
 	e8:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e8:SetCode(EVENT_LEAVE_FIELD)
-	e8:SetLabel(1-tp)
 	e8:SetOperation(c900000092.leaveop)
 	e8:SetReset(RESET_EVENT+RESET_TURN_SET+RESET_TOFIELD+RESET_OVERLAY)
 	c:RegisterEffect(e8)
@@ -140,7 +141,7 @@ function c900000092.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(1-tp) and c900000092.filter(chkc,e,tp) end
 	if chk==0 then return e:GetHandler():IsRelateToEffect(e) and e:GetHandler():IsFaceup() end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c900000092.filter,tp,0,LOCATION_GRAVE,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c900000092.filter2,tp,0,LOCATION_GRAVE,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c900000092.spop(e,tp,eg,ep,ev,re,r,rp)
@@ -154,6 +155,11 @@ end
 function c900000092.filter(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP)
 end
+
+function c900000092.filter2(c)
+	return c:IsType(TYPE_MONSTER)
+end
+
 function c900000092.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and c900000092.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c900000092.filter,tp,0,LOCATION_ONFIELD,1,nil) end
@@ -170,7 +176,7 @@ end
 
 function c900000092.leaveop(e,tp,eg,ep,ev,re,r,rp)
 	local WIN_REASON_RELAY_SOUL=0x1a
-	Duel.Win(e:GetLabel(),WIN_REASON_RELAY_SOUL)
+	Duel.Win(1-tp,WIN_REASON_RELAY_SOUL)
 end
 
 
