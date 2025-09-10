a069ab0bdbc085879f2424c381009f544012f477
jinyouli <229338934@qq.com>
2025-09-11 07:18:12 +0800

    记录

--- script/c511000920.lua
+++ script/c511000920.lua
@@ -12,37 +12,61 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return re:IsActiveType(TYPE_SPELL) and re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev) and ep~=tp
+	return Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2
 end
+
+function s.filter(c)
+    return c:IsType(TYPE_SPELL) and c:IsFaceup() and c:GetOriginalCode()~=id
+end
+
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
+
 	Duel.NegateEffect(ev)
-	local c=e:GetHandler()
 	local tc=re:GetHandler()
+	local c=e:GetHandler()
 	local tpe=tc:GetType()
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CHANGE_TYPE)
-	e1:SetValue(tpe)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
-	c:RegisterEffect(e1)
-	local te=tc:GetActivateEffect()
-	local tg=te:GetTarget()
-	local op=te:GetOperation()
-	e:SetCategory(te:GetCategory())
-	e:SetProperty(te:GetProperty())
-	Duel.ClearTargetCard()
-	if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
-	Duel.BreakEffect()
-	if op then op(e,tp,eg,ep,ev,re,r,rp) end
+	
 	if tc:IsType(TYPE_EQUIP+TYPE_CONTINUOUS) then
-		local code=tc:GetOriginalCode()
-		c:CopyEffect(code,RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET,1)
+
+		-- 获取目标卡的原始代码
+		local code = tc:GetOriginalCode()
+		
+		-- 复制永续魔法的效果
+		local cid = c:CopyEffect(code, RESET_EVENT+RESETS_STANDARD, 1)
 		c:CancelToGrave()
+		
+		-- 记录复制的效果
+		local e01 = Effect.CreateEffect(c)
+		e01:SetType(EFFECT_TYPE_SINGLE)
+		e01:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+		e01:SetCode(EFFECT_CHANGE_CODE)
+		e01:SetValue(123111)
+		e01:SetReset(RESET_EVENT+RESETS_STANDARD)
+		c:RegisterEffect(e01)
+		
+	else
+
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_TYPE)
+		e1:SetValue(tpe)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+		c:RegisterEffect(e1)
+		local te=tc:GetActivateEffect()
+		local tg=te:GetTarget()
+		local op=te:GetOperation()
+		e:SetCategory(te:GetCategory())
+		e:SetProperty(te:GetProperty())
+		Duel.ClearTargetCard()
+		if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
+		Duel.BreakEffect()
+		if op then op(e,tp,eg,ep,ev,re,r,rp) end
 	end
+
 	if (tpe&TYPE_FIELD)~=0 then
 		local of=Duel.GetFieldCard(1-tp,LOCATION_FZONE,0)
 		if of then Duel.Destroy(of,REASON_RULE) end
