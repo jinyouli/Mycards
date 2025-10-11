d66b83be20f976643dcf6287b1ec27231c3e5226
jinyouli <229338934@qq.com>
2025-10-11 18:31:30 +0800

    Update c77240339.lua

--- script/c77240339.lua
+++ script/c77240339.lua
@@ -3,7 +3,7 @@ local s, id = GetID()
 function s.initial_effect(c)
     --fusion material
     c:EnableReviveLimit()
-    Fusion.AddProcMix(c, true, true, 53183600, 90960358)
+    aux.AddFusionProcCode2(c,53183600,90960358,true,true)
     --spsummon
     local e1 = Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id, 0))
