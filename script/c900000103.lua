79cdf5ba2a322e3b1d74c4631361fd1ec3d4c3be
jinyouli <229338934@qq.com>
2025-10-17 17:46:48 +0800

    病毒卡

---
+++ script/c900000103.lua
@@ -0,0 +1,42 @@
+--死のデッキ破壊ウイルス
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_TOHAND)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.costfilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsAttackBelow(1000)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,c57728570.costfilter,1,nil) end
+	local g=Duel.SelectReleaseGroup(tp,c57728570.costfilter,1,1,nil)
+	Duel.Release(g,REASON_COST)
+end
+function s.tgfilter(c)
+	return c:IsFaceup() and c:GetAttack()>=1500 and c:IsDestructable()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(s.tgfilter,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.filter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAttackAbove(1500)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local conf=Duel.GetFieldGroup(tp,0,LOCATION_MZONE+LOCATION_HAND+LOCATION_DECK)
+	if #conf>0 then
+		Duel.ConfirmCards(tp,conf)
+		local dg=conf:Filter(s.filter,nil)
+		Duel.Destroy(dg,REASON_EFFECT)
+		Duel.ShuffleHand(1-tp)
+	end
+end
