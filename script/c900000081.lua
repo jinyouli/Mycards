379c256af0dc35bdf98a0f82765b8cebd37ebcbc
jinyouli <229338934@qq.com>
2025-11-26 01:16:02 +0800

    大自然竹笋

--- script/c900000081.lua
+++ script/c900000081.lua
@@ -1,40 +1,40 @@
 --naturia bamboo shoot
 function c900000081.initial_effect(c)
-
-    --cannot be target/effect indestructable
+	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(aux.tgoval)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(c900000081.spcon)
 	c:RegisterEffect(e1)
 
-	--special summon
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_SPSUMMON_PROC)
-	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e2:SetRange(LOCATION_HAND)
-	e2:SetCondition(c900000081.spcon)
-	c:RegisterEffect(e2)
-
 	--不会被战斗破坏
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-    e3:SetValue(1)
-    c:RegisterEffect(e3)
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
 
 	--limit spell trap
-	local e7=Effect.CreateEffect(c)
-	e7:SetType(EFFECT_TYPE_FIELD)
-	e7:SetRange(LOCATION_MZONE)
-	e7:SetCode(EFFECT_CANNOT_ACTIVATE)
-	e7:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e7:SetTargetRange(0,1)
-	e7:SetValue(c900000081.aclimit)
-	c:RegisterEffect(e7)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(0,1)
+	e3:SetValue(c900000081.aclimit)
+	c:RegisterEffect(e3)
+
+	--limit monster
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetTargetRange(0,1)
+	e4:SetValue(c900000081.acmonsterlimit)
+	c:RegisterEffect(e4)
 end
 
 function c900000081.spcon(e,c)
@@ -44,4 +44,8 @@ end
 
 function c900000081.aclimit(e,re,tp)
 	return re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+
+function c900000081.acmonsterlimit(e,re,tp)
+	return re:IsActiveType(TYPE_MONSTER)
 end
