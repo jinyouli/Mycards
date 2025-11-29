f838d77b1c3dccc6d68f21bd5dfa19651973b312
jinyouli <229338934@qq.com>
2025-11-29 11:07:00 +0800

    神史莱姆+闪烁龙+殉道者逆转

---
+++ script/c511001957.lua
@@ -0,0 +1,40 @@
+--閃珖竜 スターダスト (Anime)
+--Stardust Spark Dragon (Anime)
+--閃珖竜 スターダスト
+function c511001957.initial_effect(c)
+	--synchro summon
+	aux.AddSynchroProcedure(c,nil,aux.NonTuner(nil),1)
+	c:EnableReviveLimit()
+	--indes
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(83994433,0))
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c511001957.target)
+	e1:SetOperation(c511001957.operation)
+	c:RegisterEffect(e1)
+end
+function c511001957.filter(c)
+	return c:IsFaceup()
+end
+function c511001957.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsOnField() and c511001957.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c511001957.filter,tp,LOCATION_ONFIELD,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c511001957.filter,tp,LOCATION_ONFIELD,0,1,1,nil)
+end
+function c511001957.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
+
